e980697c7c7c4c7cf9f2f4e8c232cdae963d0417
Roman Pedchenko <romanpedchenko@Romans-MacBook-Pro.local>
2022-07-22 23:35:11 +0200

    Initial commit

---
+++ test/presaldo-telegram-bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PresaldoTelegramBot from '../lib/presaldo-telegram-bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/presaldo-telegram-bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PresaldoTelegramBot.PresaldoTelegramBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
