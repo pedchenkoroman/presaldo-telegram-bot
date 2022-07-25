6cdf2bf955c335d34ee695d3b696d53d33d7fff1
pedchekoroman <roman.pedchenko.88@gmail.com>
2022-07-25 23:46:17 +0200

    add timeout to checkBalance lambda, and implement check and update balance

--- src/check-balance.ts
+++ src/check-balance.ts
@@ -1,24 +1,101 @@
 import * as AWS from 'aws-sdk';
 const chromium = require('@sparticuz/chrome-aws-lambda');
+import { fromPairs, curry, compose, propOr, head } from 'ramda';
+
 const TABLE_NAME = process.env.TABLE_NAME || '';
 const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
 
 const db = new AWS.DynamoDB.DocumentClient();
 
-export const handler = async (event: any = {}): Promise<any> => {
-  const params = {
-    TableName: TABLE_NAME,
-  };
+type User = {
+  secureCode: string;
+  accountId: string;
+  accountNumber: string;
+  balance: number;
+};
 
+export const handler = async (): Promise<any> => {
   try {
-    const response = await db.scan(params).promise();
-    if (response.Items) {
-      console.log('response.Items', response.Items);
-      return { statusCode: 200, body: JSON.stringify(response.Items) };
-    } else {
-      return { statusCode: 404 };
+    const user = (await db
+      .scan({
+        TableName: TABLE_NAME,
+      })
+      .promise()
+      .then(compose(head, propOr([], 'Items')))) as User | undefined;
+
+    if (!user) {
+      return { statusCode: 500, body: JSON.stringify('User is empty') };
     }
-  } catch (dbError) {
-    return { statusCode: 500, body: JSON.stringify(dbError) };
+
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: {
+        width: 1024,
+        height: 800,
+      },
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.prepaidsaldo.com');
+
+    const fillField = async ([selector, value]: string[]): Promise<void> => {
+      const field = await page.$(selector);
+      return field.type(value);
+    };
+    for (const item of [
+      ['#mainform\\:cardnumber', user.accountNumber],
+      ['#mainform\\:password', user.secureCode],
+    ]) {
+      await fillField(item);
+    }
+
+    const nextBtn = await page.$('a.last');
+    await nextBtn.click();
+    await page.waitForSelector('#scrollable-content');
+
+    const table = await page.$('table.data');
+    const res = await table.$$eval('tr', (node: Array<any>) =>
+      node.map((n) => n.innerText).map((text) => text.split('\t')),
+    );
+    await browser.close();
+
+    const takeSecond = ([, second]: any) => second;
+    const slice = curry((beginIndex, str) => str.slice(beginIndex));
+    const currentBalance = compose(
+      Number,
+      slice(2),
+      takeSecond,
+      Object.values,
+      fromPairs,
+    )(res);
+
+    const { balance, accountId, ...rest } = user;
+    if (balance === currentBalance) {
+      return { statusCode: 204, body: 'Nothing changes' };
+    }
+    const params: any = {
+      TableName: TABLE_NAME,
+      Key: {
+        [PRIMARY_KEY]: accountId,
+      },
+      UpdateExpression: 'set balance = :balance',
+      ExpressionAttributeValues: {},
+      ReturnValues: 'UPDATED_NEW',
+    };
+    params.ExpressionAttributeValues[`:balance`] = currentBalance;
+
+    ['secureCode', 'accountNumber'].forEach((property) => {
+      params.UpdateExpression += `, ${property} = :${property}`;
+      // @ts-ignore
+      params.ExpressionAttributeValues[`:${property}`] = rest[property];
+    });
+
+    await db.update(params).promise();
+    return { statusCode: 204, body: 'The balance was updated' };
+  } catch (error) {
+    console.log('error', error);
+    return { statusCode: 500, body: JSON.stringify('Something went wrong') };
   }
 };
