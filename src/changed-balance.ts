fbe97e56e84d78a45f373a0abda0ef39e03f5d7d
Roman Pedchenko <roman.pedchenko.88@gmail.com>
2022-07-26 23:54:57 +0200

    Changed balance lambda (#2)
    * edit schedule expression, add telegraf package and implement changed-balance lambda logic.
    
    * Add try catch construction
    
    * Add dotenv package, create .env.example

---
+++ src/changed-balance.ts
@@ -0,0 +1,31 @@
+import { Telegraf } from 'telegraf';
+
+const TOKEN = process.env.BOT_TOKEN || '';
+
+export const handler = async (event: any = {}): Promise<any> => {
+  const {
+    Records: [record],
+  } = event;
+  if (record.dynamodb?.NewImage?.accountId?.N) {
+    console.log('Account id does not exist', record.dynamodb);
+    return {
+      statusCode: 500,
+      body: JSON.stringify('Account id does not exist'),
+    };
+  }
+  const bot = new Telegraf(TOKEN);
+
+  try {
+    await bot.telegram.sendMessage(
+      record.dynamodb.NewImage.accountId.N,
+      `Balance was changed and right now equals **${record.dynamodb.NewImage?.balance?.N}** euro`,
+    );
+    await bot.launch();
+  } catch (e) {
+    console.log('error', JSON.stringify(e));
+    return {
+      statusCode: 500,
+      body: JSON.stringify('Something went wrong'),
+    };
+  }
+};
